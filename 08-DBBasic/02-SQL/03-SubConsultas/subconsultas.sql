5bfb6a46f4f81ebe605ed050bd20ac72f662fa31
JuanFernandezJubin <fernandezjubin.data@gmail.com>
2020-12-14 22:30:04 -0300

    ejercitando0

--- 08-DBBasic/02-SQL/03-SubConsultas/subconsultas.sql
+++ 08-DBBasic/02-SQL/03-SubConsultas/subconsultas.sql
@@ -14,3 +14,39 @@ WHERE id NOT IN(
     SELECT usuario_id 
     FROM posts
 );
+
+# Sacar usuarios que tengan alguna entrada que en su titulo hable de GTA
+SELECT nombre,apellidos 
+FROM usuarios 
+WHERE id IN(
+    SELECT usuario_id 
+    FROM posts 
+    WHERE titulo 
+    LIKE '%GTA%'
+    
+);
+
+# Sacar todas los posts de una categoría en concreto utilizando su nombre Acción
+SELECT * 
+FROM posts 
+WHERE categoria_id IN(
+    SELECT id 
+    FROM categorias
+    WHERE nombre = 'Acción'
+); 
+
+# Mostrar las categorías con mas de 2 POSTS
+SELECT nombre 
+FROM categorias 
+WHERE id IN (
+    SELECT categoria_id
+    FROM POSTS 
+    GROUP BY categoria_id
+    HAVING COUNT(categoria_id) >= 2
+);
+
+# Mostrar los usuarios que crearon un posts un Martes
+
+# Mostrar el nombre del usuario que tenga mas posts
+
+# Mostrar las categorías sin posts
